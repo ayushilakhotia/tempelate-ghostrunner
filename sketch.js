e1376710e6bec3e5396a8a92feba1d3e0890662e
ayushilakhotia <77203645+ayushilakhotia@users.noreply.github.com>
2022-07-29 13:25:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+function preload(){
+  
+}
+
+function setup(){
+  createCanvas(600,600);
+  //spookySound.loop();
+ 
+  
+}
+
+function draw(){
+  background(0);
+  
+    
+    
+    //climbersGroup.collide(ghost);
+   
+    drawSprites();
+}
+  
+ 
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+ 
+   
+    //assign lifetime to the variable
+    
+    
+    //add each door to the group
+    
+}
+
